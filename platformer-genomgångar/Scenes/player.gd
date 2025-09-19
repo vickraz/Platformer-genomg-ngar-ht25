cca1ef48c0f41dbe670e9fade7c500abc820cc74
vickraz <victor.oberg@minervagymnasium.se>
2025-09-19 09:30:55 +0200

    edge movement added

--- platformer-genomgångar/Scenes/player.gd
+++ platformer-genomgångar/Scenes/player.gd
@@ -10,11 +10,13 @@ enum{IDLE, WALK, AIR, EDGE, DEAD}
 var state = IDLE
 var want_to_jump: bool = false
 var jump_buffer: float = 0.0
+var edge_input_x: float = 0.0
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var anim: AnimationPlayer = $AnimationPlayer
-
-
+@onready var left_ray: RayCast2D = $LeftRay
+@onready var right_ray: RayCast2D = $RightRay
+@onready var edge_timer: Timer = $EdgeStateTimer
 
 ############### GAME LOOP ##############################
 func _physics_process(delta: float) -> void:
@@ -32,13 +34,23 @@ func _physics_process(delta: float) -> void:
 
 ############### GENERAL HELP FUNCTIONS ########################
 func _movement(delta: float, input_x: float) -> void:
-	if input_x != 0:
-		velocity.x =  move_toward(velocity.x, input_x*MAX_SPEED, ACC*delta)
+	if up_direction.is_equal_approx(Vector2.UP) or up_direction.is_equal_approx(Vector2.DOWN):
+		if input_x != 0:
+			velocity.x =  move_toward(velocity.x, input_x*MAX_SPEED*(-sin(up_direction.angle())), ACC*delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, ACC*delta)
+			
+		velocity.y += -up_direction.y * GRAVITY * delta
+		apply_floor_snap()
+		move_and_slide()
 	else:
-		velocity.x = move_toward(velocity.x, 0, ACC*delta)
-	velocity.y += GRAVITY * delta
-	
-	move_and_slide()
+		if input_x != 0:
+			velocity.y =  move_toward(velocity.y, input_x*MAX_SPEED*(cos(up_direction.angle())), ACC*delta)
+		else:
+			velocity.y = move_toward(velocity.y, 0, ACC*delta)
+		velocity.x += -up_direction.x * GRAVITY * delta
+		apply_floor_snap()
+		move_and_slide()
 
 func _update_direction(input_x: float) -> void:
 	if input_x > 0:
@@ -52,7 +64,12 @@ eller inte. Funktionen ska returnera "left", "right" eller "" beroende på.
 Detta med hjälp av Raycasts (LeftRay och RightRay)
 """
 func _on_edge() -> String:
-	return ""
+	if left_ray.is_colliding() and not right_ray.is_colliding():
+		return "right"
+	elif not left_ray.is_colliding() and right_ray.is_colliding():
+		return "left"
+	else:
+		return ""
 
 
 ############### STATE FUNCTIONS ########################
@@ -65,9 +82,12 @@ func _idle_state(delta: float) -> void:
 	#2
 	_movement(delta, input_x)
 	#3
-	if not is_on_floor():
-		_enter_air_state(false)
-	elif velocity.length() > 0:
+	var edge = _on_edge()
+	if not is_on_floor() and edge == "left" and state != AIR:
+		_enter_edge_state("left")
+	elif not is_on_floor() and edge == "right" and state != AIR:
+		_enter_edge_state("right")
+	elif velocity.length() > 0 and state != AIR:
 		_enter_walk_state()
 
 func _walk_state(delta: float) -> void:
@@ -79,10 +99,13 @@ func _walk_state(delta: float) -> void:
 	#2
 	_movement(delta, input_x)
 	#3
-	if velocity.length() == 0:
+	var edge = _on_edge()
+	if not is_on_floor() and edge == "left" and state != AIR:
+		_enter_edge_state("left")
+	elif not is_on_floor() and edge == "right" and state != AIR:
+		_enter_edge_state("right")
+	elif velocity.length() == 0:
 		_enter_idle_state()
-	elif not is_on_floor():
-		_enter_air_state(false)
 
 func _air_state(delta: float) -> void:
 	#1
@@ -108,7 +131,7 @@ func _air_state(delta: float) -> void:
 		
 
 func _edge_state(delta: float) -> void:
-	pass
+	_movement(delta, edge_input_x)
 
 func _dead_state(delta: float) -> void:
 	pass
@@ -128,10 +151,28 @@ func _enter_air_state(jumping: bool):
 	want_to_jump = false
 	jump_buffer = 0.0
 	if jumping:
-		velocity.y = -JUMP_VELOCITY
+		velocity += up_direction*JUMP_VELOCITY
 
 func _enter_edge_state(rotation_direction: String):
-	pass
+	state = EDGE
+	floor_snap_length = 50
+	edge_timer.start()
+	var tween = get_tree().create_tween()
+	if rotation_direction == "left":
+		edge_input_x = -1
+		up_direction = up_direction.rotated(-PI/2)
+		tween.tween_property(self, "rotation", rotation - PI/2, 0.1)
+	else:
+		edge_input_x = 1
+		up_direction = up_direction.rotated(PI/2)
+		tween.tween_property(self, "rotation", rotation + PI/2, 0.1)
 	
 func _enter_dead_state():
 	pass
+
+
+################## SIGNALS #############################
+
+func _on_edge_state_timer_timeout() -> void:
+	_enter_walk_state()
+	floor_snap_length = 5
