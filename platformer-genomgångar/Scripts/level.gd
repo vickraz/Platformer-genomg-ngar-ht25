603ecf060acbfdd86abed0e4354f80195c70f98b
vickraz <victor.oberg@minervagymnasium.se>
2025-10-14 15:28:01 +0200

    highscore system added

--- platformer-genomgångar/Scripts/level.gd
+++ platformer-genomgångar/Scripts/level.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const SAVE_PATH = "user://gravityplatformer_savefile.data"
 
 @onready var camera: Camera2D = $Camera2D
 @onready var player: Player = $Player
@@ -10,6 +11,7 @@ const PLAYER_SCENE = preload("res://Scenes/player.tscn")
 
 var time: float = 0.0
 var level_completed: bool = false
+var highscores: Dictionary = {}
 
 @export var level = 1
 
@@ -17,6 +19,8 @@ func _ready() -> void:
 	player.get_node("RemoteTransform2D").remote_path = camera.get_path()
 	player.connect("dead", _on_Player_dead)
 	heart.connect("pickup", _on_Heart_pickup)
+	_get_highscores()
+	print(highscores)
 
 func _process(delta: float) -> void:
 	if not level_completed:
@@ -26,8 +30,6 @@ func _process(delta: float) -> void:
 		var sec = int(time - min*60)
 		
 		time_label.text = "Time: " + "%02d:%02d" % [min, sec]
-	
-	
 
 
 func _on_Player_dead() -> void:
@@ -40,3 +42,26 @@ func _on_Player_dead() -> void:
 func _on_Heart_pickup():
 	level_completed = true
 	LevelManager.change_to_next_level(level)
+	
+	if name in highscores:
+		#Ett befintligt highscore finns
+		if time < highscores[name]:
+			_save_highscore(name)
+	else:
+		#Om den inte finns i highscores är det första gången leveln klaras -> highscore
+		_save_highscore(name)
+	
+
+func _get_highscores() -> void:
+	if FileAccess.file_exists(SAVE_PATH):
+		var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+		highscores = file.get_var() #Hämtar den sparade variabeln
+		file.close()
+	
+
+func _save_highscore(level_name: String) -> void:
+	highscores[level_name] = time #Ändrar på värdet om det finns eller lägger till om det inte finns
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE) #Finns ej filen skapas den automatiskt
+	file.store_var(highscores)
+	file.close()
+	
