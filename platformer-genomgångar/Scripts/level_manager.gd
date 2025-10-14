7b48ea7cee9a41bf1fda5eb38cb1d7f08fa39d8b
vickraz <victor.oberg@minervagymnasium.se>
2025-10-14 09:41:41 +0200

    level transition added

--- platformer-genomgångar/Scripts/level_manager.gd
+++ platformer-genomgångar/Scripts/level_manager.gd
@@ -4,9 +4,16 @@ extends Node2D
 const LAST_LEVEL = 2
 const LEVEL_PATH = "res://Scenes/level_"
 
+@onready var anim: AnimationPlayer = $AnimationPlayer
 
 func change_to_next_level(current_level:int) -> void:
 	if current_level < LAST_LEVEL:
+		anim.play("fade_in")
+		await anim.animation_finished
 		get_tree().change_scene_to_file(LEVEL_PATH + str(current_level + 1) + ".tscn")
+		anim.play("fade_out")
 	else:
+		anim.play("fade_in")
+		await anim.animation_finished
 		get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+		anim.play("fade_out")
